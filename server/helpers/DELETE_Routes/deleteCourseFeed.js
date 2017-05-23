33d579f49cf60eac02e52d728418aa79292d302c
Behzad-Az <behy1368@gmail.com>
2017-05-22 19:10:49 -0700

    fixes #18

--- server/helpers/DELETE_Routes/deleteCourseFeed.js
+++ server/helpers/DELETE_Routes/deleteCourseFeed.js
@@ -1,17 +1,30 @@
 const deleteCourseFeed = (req, res, knex, user_id) => {
 
-  const deleteCourseFeed = () => knex('course_feed')
+  const deleteCourseFeed = trx => knex('course_feed')
+    .transacting(trx)
     .where('id', req.params.feed_id)
     .andWhere('commenter_id', user_id)
     .andWhere('course_id', req.params.course_id)
+    .andWhere('category', 'new_comment')
+    .andWhere('header', 'new_comment')
     .del();
 
-  deleteCourseFeed()
+  const deleteFeedReplies = trx => knex('course_feed_replies')
+    .transacting(trx)
+    .where('course_feed_id', req.params.feed_id)
+    .del();
+
+  knex.transaction(trx => {
+    deleteFeedReplies(trx)
+    .then(() => deleteCourseFeed(trx))
+    .then(() => trx.commit())
+    .catch(err => {
+      console.error('Error isnide deleteCourseFeed.js: ', err);
+      trx.rollback();
+    });
+  })
   .then(() => res.send(true))
-  .catch(err => {
-    console.error('Error inside deleteCourseFeed.js: ', err);
-    res.send(false);
-  });
+  .catch(err => res.send(false));
 
 };
 
