a4a100f6abad4f469412b14b7f71d6d1047002c6
Behzad-Az <behy1368@gmail.com>
2017-05-23 07:26:28 -0700

    updated proxy ip address

--- client/cfg/base.js
+++ client/cfg/base.js
@@ -24,9 +24,9 @@ module.exports = {
     historyApiFallback: true,
     // hot: true,
     proxy: {
-      '/api/**': 'http://192.241.217.135:19001',
-      '/api/': 'http://192.241.217.135:19001',
-      '/imagesapi/': 'http://192.241.217.135:19001'
+      '/api/**': 'http://198.199.115.67:19001',
+      '/api/': 'http://198.199.115.67:19001',
+      '/imagesapi/': 'http://198.199.115.67:19001'
     },
     port: defaultSettings.port,
     publicPath: defaultSettings.publicPath,
